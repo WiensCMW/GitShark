df3a7821dfba8186195924eafcec996c5973fd76
Cornie Wiens <cornie.wiens@vidir.com>
2024-08-15 14:25:13 -0500

    Initial project commit

---
+++ GitShark/GitShark/Program.cs
@@ -0,0 +1,15 @@
+﻿using Spectre.Console;
+
+namespace GitShark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AnsiConsole.Write(new FigletText("GitShark!").Centered());
+            AnsiConsole.Write(new Rule("Git with Teeth (not really)").Centered());
+
+            Console.ReadKey();
+        }
+    }
+}
